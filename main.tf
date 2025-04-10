a767326a7e0ede1df8cacc655d82756458ecf67e
probeike <agouws@macbookpro.mynetworksettings.com>
2025-04-10 16:59:19 -0400

    Configured terraform to deploy to aws account

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+variable "aws_account_id" {
+  type = string
+  description = "The AWS account ID used to make resources globally unique"
+}
+
 resource "aws_s3_bucket" "starter-bucket" {
-  bucket = "starter-bucket"
+  bucket = "starter-bucket-${var.aws_account_id}"
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
   region = "us-east-1"
-  # AWS credentials are expected to be provided via environment variables:
-  # AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
 }
